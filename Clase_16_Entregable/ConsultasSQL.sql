06be9653decda0d50ec24fa94d6281951edabdd2
TomasCostilla <arg.myapps@gmail.com>
2021-08-13 17:06:19 -0300

    commit

---
+++ Clase_16_Entregable/ConsultasSQL.sql
@@ -0,0 +1,9 @@
+insert into items (nombre,categoria,stock) values ('Fideos','Harina',20);
+insert into items (nombre,categoria,stock) values ('Leche','Lacteos',30);
+insert into items (nombre,categoria,stock) values ('Crema','Lacteos',15);
+
+select * from items
+
+delete from items where id = 1
+
+update items set stock = 45 where id = 2
